22994a45d93c21deebb899b860c5cf0500b6bf0b
Sammie Sayers <sammy@fiks.online>
2023-03-30 22:47:08 +0200

    register en login routing

--- router.js
+++ router.js
@@ -3,7 +3,11 @@ const router = express();
 let path = require('path');
 router.set('view engine', 'ejs');
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const PORT = 3000;
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 router.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
 router.use('/styles', express.static(path.join(__dirname, 'styles')));
@@ -87,3 +91,68 @@ router.post('/editProduct/:id', function (req, res, next) {
         });
 });
 
+
+
+
+
+router.get('/login', (req, res) => {
+    res.render("login.ejs")
+})
+
+router.get('/register', (req, res) => {
+    res.render("register.ejs")
+})
+
+
+router.post('/login', function (req, res, next) {
+    const { EMail } = req.body;
+
+    // Make a GET request to retrieve the list of users from the external API
+    fetch('https://roc.tngapps.com/TPWQ283/users', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then(async response => {
+            const users = await response.json();
+            const user = users.find(u => u.EMail === EMail);
+
+            if (user) {
+                // If user is found, redirect to index page or perform some other action
+                res.redirect('/');
+            } else {
+                // If user is not found, render the login page with an error message
+                res.render('login', { errorMessage: 'Invalid email or password.' });
+            }
+        })
+        .catch(error => {
+            res.render('error', { message: error });
+        });
+});
+
+router.post('/register', function (req, res, next) {
+
+    const {Name, EMail, Password} = req.body
+    if (!Name || !EMail || !Password) {
+
+        res.redirect('/register');
+        return;
+    }
+
+    let newUser = {
+         Name: req.body.Name,
+         EMail: req.body.EMail,
+         Password: req.body.Password,
+         UserLevel: 1,
+    }; console.log(newUser)
+    fetch('https://roc.tngapps.com/' + 'TPWQ283' + '/users', {
+        method: 'POST',
+        body: JSON.stringify(newUser),
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then(async response => {
+            res.redirect('/login');
+        })
+        .catch(error => {
+            res.render('error', { message: error });
+        });
+});
